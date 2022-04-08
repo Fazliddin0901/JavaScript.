a932203d5ea08ede6d5272e8673f7539e63533cf
Fazliddin Umirzoqov <fazliddinumirzoqov0122@gmail.com>
2022-04-08 18:19:33 +0500

    chcd

---
+++ NPM/login.js
@@ -0,0 +1,16 @@
+// let a = 5;
+// let t = 19;
+// export { a, t as alla };
+
+// // neme orqali export
+
+// let login1 = 'umid';
+// let password = 11111;
+
+// export function login(login, parol) {
+//   if (login === login1 && parol === password) {
+//     console.log('tizimga kirding');
+//   } else {
+//     console.log('kirolmading');
+//   }
+// }
---
+++ NPM/script.js
@@ -0,0 +1,32 @@
+// import * as hammasi from './login.js';
+// console.log(hammasi.aln n la);
+
+// import { login } from './login.js';
+// login('umid', 1111);
+
+// console.log('Hello');
+
+// async function ol() {
+//   const country = await fetch('https://restcountries.com/v2/all');
+//   const countryJson = await country.json();
+//   console.log(countryJson);
+// }
+// ol();
+
+// console.log('Hello1');
+
+let sot = (function () {
+  let cart = [];
+  let maxsulotSoni = 100;
+  let ishchilarSoni = 10;
+  let hammaPul = 1_000_000;
+
+  function sotibOl(nomi, soni) {
+    cart.push({ nomi, soni });
+    console.log(`Siz ${nomi}ni ${soni} ta oldingiz`);
+  }
+  return { maxsulotSoni, ishchilarSoni, sotibOl };
+})();
+
+console.log(sot);
+sot.sotibOl('olma', 10);
